851e68e0765fcfb09c00211a4d8f8fb05f2be7cc
JSisques <javierplazasisques@gmail.com>
2025-11-26 22:04:18 +0100

    feat: Update Next.js configuration to enable standalone output for improved deployment

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/shared/i18n/request.ts");
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
   images: {
     remotePatterns: [
       {
